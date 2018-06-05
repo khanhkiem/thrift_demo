200c7f83e5bed1217443934bbd0b1343dc96f524
khanhkiem <buidoigiauco@gmail.com>
2018-06-05 11:46:54 +0700

    done demo thrift

---
+++ shared.thrift
@@ -0,0 +1,7 @@
+namespace java shared
+namespace py shared
+
+struct SharedStruct {
+  1: i32 key,
+  2: string value,
+}
---
+++ src/tutorial/MultiplicationHandler.java
@@ -0,0 +1,15 @@
+package tutorial;
+
+
+import org.apache.thrift.TException;
+import tutorial.MultiplicationService;
+
+public class MultiplicationHandler implements MultiplicationService.Iface {
+
+    @Override
+    public int multiply(int n1, int n2) throws TException {
+        System.out.println("Multiply(" + n1 + "," + n2 + ")");
+        return n1 * n2;
+    }
+
+}
